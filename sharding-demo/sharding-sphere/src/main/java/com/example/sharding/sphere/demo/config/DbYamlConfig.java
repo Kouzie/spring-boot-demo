d172569e05c7d29d72d8faefa24b8fd1a35ea02e
kouzie <kgy1996@naver.com>
2024-07-18 02:30:00 +0900

    update file path code for jar

--- sharding-demo/sharding-sphere/src/main/java/com/example/sharding/sphere/demo/config/DbYamlConfig.java
+++ sharding-demo/sharding-sphere/src/main/java/com/example/sharding/sphere/demo/config/DbYamlConfig.java
@@ -1,25 +1,14 @@
 package com.example.sharding.sphere.demo.config;
 
-import com.zaxxer.hikari.HikariDataSource;
-import org.apache.shardingsphere.driver.api.ShardingSphereDataSourceFactory;
 import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
-import org.apache.shardingsphere.infra.config.algorithm.AlgorithmConfiguration;
-import org.apache.shardingsphere.sharding.algorithm.keygen.SnowflakeKeyGenerateAlgorithm;
-import org.apache.shardingsphere.sharding.api.config.ShardingRuleConfiguration;
-import org.apache.shardingsphere.sharding.api.config.rule.ShardingTableRuleConfiguration;
-import org.apache.shardingsphere.sharding.api.config.strategy.sharding.NoneShardingStrategyConfiguration;
-import org.apache.shardingsphere.sharding.api.config.strategy.sharding.StandardShardingStrategyConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.sql.DataSource;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Properties;
 
 @Configuration
 public class DbYamlConfig {
@@ -31,8 +20,8 @@ public class DbYamlConfig {
 
     @Bean
     public DataSource dataSource() throws SQLException, IOException {
-        ClassPathResource resource = new ClassPathResource("sharding-datasource-custom.yaml"); // Indicate YAML file
-        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(resource.getFile());
+        InputStream resource = new ClassPathResource("sharding-datasource-custom.yaml").getInputStream();
+        DataSource dataSource = YamlShardingSphereDataSourceFactory.createDataSource(resource.readAllBytes());
         return dataSource;
     }
 }
