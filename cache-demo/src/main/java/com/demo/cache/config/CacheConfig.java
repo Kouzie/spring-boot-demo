adef1ae79a7280056f70d5573c9c4ace9e83de0a
Giyong Ko <kgy1996@naver.com>
2025-08-16 16:36:24 +0900

    caffein 캐시 추가

--- cache-demo/src/main/java/com/demo/cache/config/CacheConfig.java
+++ cache-demo/src/main/java/com/demo/cache/config/CacheConfig.java
@@ -1,23 +1,20 @@
 package com.demo.cache.config;
 
-import org.ehcache.PersistentCacheManager;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.ehcache.config.CacheConfiguration;
 import org.ehcache.config.builders.CacheConfigurationBuilder;
-import org.ehcache.config.builders.CacheManagerBuilder;
 import org.ehcache.config.builders.ExpiryPolicyBuilder;
 import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.config.units.EntryUnit;
 import org.ehcache.config.units.MemoryUnit;
 import org.ehcache.core.config.DefaultConfiguration;
-import org.ehcache.expiry.ExpiryPolicy;
 import org.ehcache.impl.config.persistence.DefaultPersistenceConfiguration;
 import org.ehcache.jsr107.Eh107Configuration;
 import org.ehcache.jsr107.EhcacheCachingProvider;
-import org.ehcache.xml.XmlConfiguration;
-import org.ehcache.xml.multi.XmlMultiConfiguration;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cache.jcache.JCacheCacheManager;
 import org.springframework.context.annotation.Bean;
@@ -34,10 +31,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import javax.cache.Caching;
 import javax.cache.spi.CachingProvider;
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.time.Duration;
-import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @EnableCaching
 @Configuration
@@ -49,23 +44,34 @@ public class CacheConfig {
         return cacheManager;
     }
 
+    @Bean(name = "caffeineCacheManager")
+    public CacheManager caffeineCacheManager() {
+        Caffeine<Object, Object> defaultConfig = Caffeine.newBuilder()
+                .maximumSize(100) // 캐시 최대 항목 수
+                .expireAfterWrite(10, TimeUnit.MINUTES) // 쓰기 후 10분 뒤 만료
+                .recordStats(); // 캐시 통계 기록 (선택 사항)
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager("customerCache");
+        cacheManager.setCaffeine(defaultConfig);
+        return cacheManager;
+    }
+
     @Primary
     @Bean(name = "ehCacheManager")
-    public CacheManager cacheManager() {
+    public CacheManager ehCacheManager() {
         CachingProvider provider = Caching.getCachingProvider();
         EhcacheCachingProvider ehcacheProvider = (EhcacheCachingProvider) provider;
         DefaultConfiguration defaultConfiguration = new DefaultConfiguration(ehcacheProvider.getDefaultClassLoader(),
                 new DefaultPersistenceConfiguration(new File("cache/directory")));
         javax.cache.CacheManager cacheManager = ehcacheProvider.getCacheManager(ehcacheProvider.getDefaultURI(), defaultConfiguration);
 
-        CacheConfiguration configuration = CacheConfigurationBuilder.newCacheConfigurationBuilder(
-                Object.class, // key type
-                Object.class, // value type
-                ResourcePoolsBuilder.newResourcePoolsBuilder()
-                        .heap(1000, EntryUnit.ENTRIES)
-                        .offheap(10, MemoryUnit.MB)
-                        .disk(1, MemoryUnit.GB)
-                        .build())
+        CacheConfiguration<Object, Object> configuration = CacheConfigurationBuilder.newCacheConfigurationBuilder(
+                        Object.class, // key type
+                        Object.class, // value type
+                        ResourcePoolsBuilder.newResourcePoolsBuilder()
+                                .heap(1000, EntryUnit.ENTRIES)
+                                .offheap(10, MemoryUnit.MB)
+                                .disk(1, MemoryUnit.GB)
+                                .build())
                 .withExpiry(ExpiryPolicyBuilder.timeToLiveExpiration(Duration.ofSeconds(60)))
                 .withDefaultDiskStoreThreadPool()
                 .build();
